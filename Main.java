b35ae4d3d0ae14420833db77f29ebfccc6b8f16b
YaOn999 <yaon999@yandex.ru>
2023-08-14 18:50:18 +0300

    Initial commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+
+        list.add(5);
+        list.add(3);
+        list.add(4);
+        list.add(10);
+        list.add(2);
+        list.add(11);
+
+        list.print();
+        list.reversList();
+        list.print();
+    }
+}
